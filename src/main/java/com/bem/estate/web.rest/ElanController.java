d13e4bfb9f267f429c25281cb655224fefba6ff9
bago1 <you@example.com>
2021-12-13 03:49:26 +0400

    some homo error fixed

--- src/main/java/com/bem/estate/web.rest/ElanController.java
+++ src/main/java/com/bem/estate/web.rest/ElanController.java
@@ -1,24 +1,15 @@
 package com.bem.estate.web.rest;
 
 import com.bem.estate.constant.SwaggerConstants;
-import com.bem.estate.domain.Elan;
 import com.bem.estate.dto.ElanDto;
-import com.bem.estate.dto.SellingElanRequestDto;
 import com.bem.estate.service.ElanService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.w3c.dom.stylesheets.LinkStyle;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
